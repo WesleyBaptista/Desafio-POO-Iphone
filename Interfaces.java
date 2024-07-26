1f2b00d116a00b1c7c06eaaa6e9ec284e3c8dd7c
Wesley Baptista <wesleybaptista@live.com>
2024-07-25 23:00:15 -0300

    Implementar Classes e Interfaces em Java

---
+++ Interfaces.java
@@ -0,0 +1,17 @@
+public interface ReprodutorMusical {
+    void tocar();
+    void pausar();
+    void selecionarMusica(String musica);
+}
+
+public interface AparelhoTelefonico {
+    void ligar(String numero);
+    void atender();
+    void iniciarCorreioVoz();
+}
+
+public interface NavegadorNaInternet {
+    void exibirPagina(String url);
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
